9470ec1af86f3b2d3bbd5a78c35e0d738c1b027c
revortni <rojit.blaze13@gmail.com>
2020-01-28 09:59:42 +0545

    adjusted responsive design of forms

--- src/components/todoApp/Dashboard.js
+++ src/components/todoApp/Dashboard.js
@@ -1,5 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import NoData from './NoData';
+import '../styles/Dashboard.css';
 
-export const Dashboard = params => {
-  return <div>Apple</div>;
+const DashboardItem = props => {
+  const itemContent = props.items.map(x => <li key={x.id}>{x.text}</li>);
+  return (
+    <div className='dashboard-item'>
+      <div className='dashboard-item-title'>{props.title}</div>
+      <ul className='dashboard-item-list'>{itemContent}</ul>
+    </div>
+  );
+};
+
+export const Dashboard = props => {
+  const todoLists =
+    [
+      {
+        listId: 1,
+        title: 'Apple Collection',
+        entries: [
+          {
+            id: '123',
+            completed: false,
+            text: 'Apple'
+          },
+          {
+            id: '124',
+            completed: false,
+            text: 'Apple2'
+          }
+        ]
+      },
+      {
+        listId: 2,
+        title: 'Apple Collection',
+        entries: [
+          {
+            id: '123',
+            completed: false,
+            text: 'Apple'
+          },
+          {
+            id: '124',
+            completed: false,
+            text: 'Apple2'
+          }
+        ]
+      }
+    ] || props.todoLists;
+
+  return (
+    <div className='container'>
+      <div className='dashboard'>
+        {todoLists.length > 0 ? (
+          todoLists.map(({ entries, listId, title }) => (
+            <DashboardItem items={entries} key={listId} title={title} />
+          ))
+        ) : (
+          <NoData
+            loading={props.loading}
+            searchOn={props.filter.length ? true : false}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
