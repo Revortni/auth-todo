ade4ed33e1a148a7ac53efaaad96fd56c90986a5
revortni <rojit.blaze13@gmail.com>
2020-01-28 17:07:43 +0545

    Updates

---
+++ precache-manifest.644e9217be3d80b30890839d8870648a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa473fffe8d2ede9cd33a88a3d194f6",
+    "url": "/auth-todo/index.html"
+  },
+  {
+    "revision": "82fa8a9c5250804ae1b9",
+    "url": "/auth-todo/static/css/main.75f3dd52.chunk.css"
+  },
+  {
+    "revision": "fd1339c34961fcc0560e",
+    "url": "/auth-todo/static/js/2.ba24b9f7.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/auth-todo/static/js/2.ba24b9f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82fa8a9c5250804ae1b9",
+    "url": "/auth-todo/static/js/main.c54d5b83.chunk.js"
+  },
+  {
+    "revision": "34be0f28186c25284ed2",
+    "url": "/auth-todo/static/js/runtime-main.e76a1a52.js"
+  }
+]);
